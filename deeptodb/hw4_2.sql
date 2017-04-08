5ff45b975f53b1978764a6201522cc165636ecb3
luchnck <luchnck@yandex.ru>
2017-04-08 13:21:34 +0000

    s

--- deeptodb/hw4_2.sql
+++ deeptodb/hw4_2.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE FUNCTION SubmitReview(_paper_id INT, _reviewer_id INT, _score INT)                                                                
 RETURNS VOID AS $$                                                                                                                                  
 DECLARE                                                                                                                                             
-        n_paper_id Paper.id%TYPE;                                                                                                                        
+	n_paper_id Paper.id%TYPE;                                                                                                                        
         n_reviewer_id Reviewer.id%TYPE;                                                                                                                  
         n_score_records integer;                                                                                                                    
         t_paper_scores PaperReviewing.score%TYPE;                                                                                                        
@@ -16,16 +16,16 @@ BEGIN
                 RAISE EXCEPTION 'INPUT_DATA_NOT_VALID';                                                                                             
         END IF;                                                                                                                                     
                                                                                                                                                     
-        SELECT COUNT(*) INTO n_score_records FROM PaperReviewing PR WHERE PR.paper_id = n_paper_id AND PR.reviewer_id = n_rewiever_id;              
+        SELECT COUNT(*) INTO n_score_records FROM PaperReviewing PR WHERE PR.paper_id = n_paper_id AND PR.reviewer_id = n_reviewer_id;              
                                                                                                                                                     
         IF n_score_records > 0 THEN                                                                                                                 
-                UPDATE PaperReviewing SET score=_score WHERE paper_id = n_paper_id AND reviewer_id = n_rewiever_id;                                    
+                UPDATE PaperReviewing SET score=_score WHERE paper_id = n_paper_id AND reviewer_id = n_reviewer_id;                                    
         ELSE                                                                                                                                        
                 INSERT INTO PaperReviewing(paper_id, reviewer_id, score) VALUES (n_paper_id,n_reviewer_id,_score);                                  
         END IF;
                                                                                                                                                     
         FOR score IN 
-				SELECT score FROM PaperReviewing WHERE paper_id = n_paper_id AND reviewer_id = n_reviewer_id
+				SELECT PaperReviewing.score FROM PaperReviewing WHERE paper_id = n_paper_id AND reviewer_id = n_reviewer_id
 			LOOP                                                                                                        
                 factor := factor + score;                                                                                                           
                 cnt := cnt + 1;                                                                                                                     
@@ -36,14 +36,10 @@ BEGIN
         END IF;                                                                                                                                     
                                                                                                                                                     
         IF factor > 4 THEN                                                                                                                          
-                UPDATE Paper SET accepted VALUES (1) WHERE id = n_paper_id;                                                                         
+                UPDATE Paper SET accepted=True WHERE id = n_paper_id;                                                                         
         ELSE                                                                                                                                        
-                UPDATE Paper SET accepted VALUES (0) WHERE id = n_paper_id;                                                                         
+                UPDATE Paper SET accepted=False WHERE id = n_paper_id;                                                                         
         END IF;                                                                                                                                     
 
-EXCEPTION
-	WHEN 'INPUT_DATA_NOT_VALID' THEN
-        	RETURN 1                                                                                                                                            
-	
 END;                                                                                                                                                
 $$ LANGUAGE plpgsql;
